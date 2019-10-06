33ae4476cdcb6836f7ed816360846792319d5121
Smart Raysam <raysamtob@gmail.com>
2019-10-06 14:53:00 -0700

    Annewat

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function __construct()
+
+    {
+
+
+
+    }
+
+
+
+    /**
+
+     * Show the application dashboard.
+
+     *
+
+     * @return \Illuminate\Http\Response
+
+     */
+
+    public function admin()
+
+    {
+
+        return view('admin');
+
+    }
+
+    
+}
