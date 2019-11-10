8171c243bca63fec2e24e140fee9337675b96546
Smart Raysam <raysamtob@gmail.com>
2019-11-10 21:10:27 +0100

    success and error flashmsg update

--- app/Http/Controllers/TicketsPreviewController.php
+++ app/Http/Controllers/TicketsPreviewController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Bike_details;
+use App\Tickets;
+
 class TicketsPreviewController extends Controller
 {
     /**
@@ -49,7 +51,12 @@ class TicketsPreviewController extends Controller
         //validate the form
         $ticket = $request->session()->get('ticket');
         $bikedetail = Bike_details::where('bike_details.registrationnum',  $ticket->vehicleno)->first();
+        
+        $ticketdetail = Tickets::where('tickets.transID',  $ticket->transID)->first();
 
+        if($ticketdetail){
+            return redirect('/tickets/ticket')->with('error', 'Transaction ID already exist, Please confirm the ticket transaction number');
+        }
         if(!$bikedetail){
             return redirect('/tickets/ticket')->with('error', 'Rider does not exist, Please confirm the vehicle number');
         }else{
